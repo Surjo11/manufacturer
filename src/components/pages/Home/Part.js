8e432dbfa8a1a1b3c25d4cb0e846891e4c6ff849
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-05-25 03:23:20 +0600

    added parts details

--- src/components/pages/Home/Part.js
+++ src/components/pages/Home/Part.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Part = (props) => {
-  const { image, name, minimumquantity, availablequanity, description, price } =
-    props.part;
+  const {
+    image,
+    name,
+    minimumquantity,
+    availablequantity,
+    description,
+    price,
+  } = props.part;
   return (
     <div className="card w-auto bg-base-100 shadow-xl">
       <figure>
@@ -11,8 +17,16 @@ const Part = (props) => {
       <div className="card-body">
         <h2 className=" font-bold text-center text-2xl">{name}</h2>
         <span className="font-semibold text-center text-2xl">${price}</span>
-        <p>{description}</p>
-        <div className="card-actions justify-center">
+        <p className="text-sm text-gray-500">{description}</p>
+        <div className="flex justify-between">
+          <span className="text-lg font-medium">
+            Minimum Order Quantity: {minimumquantity}
+          </span>
+          <span className="text-lg font-medium">
+            Available Quantity: {availablequantity}
+          </span>
+        </div>
+        <div className="card-actions justify-center mt-5">
           <button className="btn bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white border-none">
             Purchase
           </button>
